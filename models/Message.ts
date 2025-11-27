6563fa85328e6c3ae9c4f3c1373da28a9f826deb
teofanov03 <teofanovdjordjenn@gmail.com>
2025-11-27 23:05:04 +0100

    dodato company u message

--- models/Message.ts
+++ models/Message.ts
@@ -4,11 +4,11 @@ const messageSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    company: { type: String, default: "N/A" },  // ← OBAVEZNO DODATO
     message: { type: String, required: true },
     status: { type: String, enum: ['unread', 'read'], default: 'unread' }
   },
   { timestamps: true }
 );
-
 
 export default mongoose.model("Message", messageSchema);
